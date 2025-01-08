3923bbe3cc5af0c58fe4e9b88f1bb4d6f49555fc
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-08 21:08:49 +0700

    render

--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../product.service';
+import { Product } from '../../type/Products';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  products: Product[] = []; // Mảng để lưu trữ danh sách sản phẩm
+  limitedProducts: Product[] = [];
+  novelCategory: Product[] = []; // Khai báo thuộc tính novelCategory
+  
+  constructor(private productService: ProductService) {}
 
+  ngOnInit(): void {
+    this.loadProducts(); // Gọi hàm để lấy dữ liệu khi component được khởi tạo
+  }
+
+  loadProducts(): void {
+    this.productService.getAll().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.limitedProducts = this.products.slice(0, 4); // Lấy 4 sản phẩm đầu tiên
+        this.filterNovelCategory(); // Lọc sản phẩm theo thể loại "Tiểu thuyết"
+      },
+      error: (err) => {
+        console.error('Error fetching products:', err);
+      }
+    });
+  }
+
+  // Hàm lọc các sản phẩm có thể loại "Tiểu thuyết"
+  filterNovelCategory(): void {
+    // Nếu product.category là một đối tượng Category, truy cập vào thuộc tính name
+    this.novelCategory = this.products.filter(product => product.category === 'Tiểu thuyết');
+  }
+  
 }
--- src/app/type/Products.ts
+++ src/app/type/Products.ts
@@ -1,9 +1,20 @@
-export type Product = {
-  id:string;
-  title:string;
-  image:string;
-  price:number;
-  dec:string;
-  category:string;
-  Active:boolean;
+export interface Product {
+  id: string;
+  title: string;
+  price: string;
+  price_after_discount: string;
+  category: string;
+  author: string;
+  description: string;
+  image_url: string;
+  rating: number;
+  reviews: {
+    user_id: string;
+    comment: string;
+    rating: number;
+  }[];
+  discount: number;
+  stock: number;
+  category_id: string;
+  publish_date: string;
 }
