15daa6d9e5ab999101a6d215d4d3aa8e169f1f15
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-13 16:42:58 +0700

    ..

--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -3,29 +3,37 @@ import { ProductService } from '../../product.service';
 import { Product } from '../../type/Products';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CartService } from '../../cart.service';
+import { WishlistService } from '../../wishlist.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
   products: Product[] = [];
+  product: Product | undefined;
+  userId: string | null = null;
   limitedProducts: Product[] = [];
   novelCategory: Product[] = [];
   romanceCategory: Product[] = [];
   fantasyCategory: Product[] = [];
   historyCategory: Product[]=[];
   educationCategory: Product[]=[]
-
+  successMessage: string | null = null;
+  alertMessage: string | null = null;
   currentIndex: number = 0;
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService,    private cartService: CartService,
+    private wishlistService: WishlistService,) {}
 
   ngOnInit(): void {
     this.loadProducts();
+    this.userId = localStorage.getItem('userId');
   }
 
   loadProducts(): void {
@@ -44,7 +52,33 @@ export class HomepageComponent implements OnInit {
       }
     });
   }
-
+  addToWishlist(): void {
+    if (this.product && this.userId) {
+      this.wishlistService.getWishlist(this.userId).subscribe({
+        next: (wishlist) => {
+          const isProductInWishlist = wishlist.some(item => item.id === this.product?.id);
+          if (isProductInWishlist) {
+            this.alertMessage = 'This product are already in your wishlist!';  // Set success message
+            setTimeout(() => {
+              this.alertMessage = null; 
+            }, 3000);
+          } else {
+            this.wishlistService.addToWishlist(this.userId!, this.product!).subscribe({
+              next: (response) => {
+                this.successMessage = 'Product added to wishlist successfully!';  // Set success message
+                setTimeout(() => {
+                  this.successMessage = null; 
+                }, 3000);
+              }
+            });
+          }
+        }
+      });
+    } else {
+      console.error('User  ID or Product is missing');
+    }
+  }
+  
   // filterNovelCategory(): void {
   //   this.novelCategory = this.products.filter(product => product.category === 'Fiction');
   // }
