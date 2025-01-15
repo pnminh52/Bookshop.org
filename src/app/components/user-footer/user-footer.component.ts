009dd798dd00cdaa5ff145724ae00261bf5d47c6
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-15 21:03:46 +0700

    footer

--- src/app/components/user-footer/user-footer.component.ts
+++ src/app/components/user-footer/user-footer.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-user-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-footer.component.html',
   styleUrl: './user-footer.component.css'
 })
