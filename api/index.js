02d496a353f27f22f58e4b93a6936c1e1e65a4f0
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-17 02:03:00 +0700

    db.json

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
-// api/index.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');  // Cập nhật đường dẫn tới db.json
 const middlewares = jsonServer.defaults();
+
+// Sử dụng cổng từ Vercel (hoặc mặc định là 3000 nếu không có)
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
