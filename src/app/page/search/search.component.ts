8e3bf25476a256cf53db93a032a32e3c721a4522
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-20 17:38:50 +0700

    search function update

--- src/app/page/search/search.component.ts
+++ src/app/page/search/search.component.ts
@@ -4,25 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+
 const routes: Routes = [
   { path: 'product/:id', component: ProductDetailComponent },
 ];
+
 @Component({
   selector: 'app-search',
   standalone: true,
   templateUrl: './search.component.html',
-  imports: [CommonModule, FormsModule,RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   styleUrls: ['./search.component.css'], 
 })
 export class SearchComponent {
   searchTerm: string = '';
   products: any[] = [];
+  isSearchInitiated: boolean = false;
 
   constructor(private productService: ProductService) {}
-  countBooks():number{
-    return this.products.length
+
+  countBooks(): number {
+    return this.products.length;
   }
+
   search() {
+    this.isSearchInitiated = true;
     if (this.searchTerm) {
       this.productService.getAll().subscribe((data: any) => {
         this.products = data.filter((product: any) => 
@@ -34,5 +40,4 @@ export class SearchComponent {
       this.products = [];
     }
   }
-
 }
