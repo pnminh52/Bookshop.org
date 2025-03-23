a2fd4dea8457bcda2e7c91b265f32d340cb14a3b
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-03-23 16:48:59 +0700

    cart ui update

--- src/app/page/cart/cart.component.ts
+++ src/app/page/cart/cart.component.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { CartService } from '../../cart.service'; 
 import { Product } from '../../type/Products';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
   cartItems: Product[] = [];
+  successMessage: string | null = null; 
  
 
   constructor(private cartService: CartService, private router: Router) {
@@ -23,6 +25,10 @@ export class CartComponent {
   removeFromCart(product: Product): void {
     this.cartService.removeFromCart(product);
     this.cartItems = this.cartService.getCartItems();
+    this.successMessage = 'Product removed from cart successfully!';
+    setTimeout(() => {
+      this.successMessage = null;
+    }, 3000);
   }
 
   increaseQuantity(product: Product): void {
@@ -40,5 +46,8 @@ export class CartComponent {
   }
   proceedToCheckout(): void {
     this.router.navigate(['/checkout']); 
+  }
+  backToCollection(): void {
+    this.router.navigate(['/collection']); 
   }
 }
